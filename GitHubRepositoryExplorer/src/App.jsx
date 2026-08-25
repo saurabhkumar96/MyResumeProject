517d1e86ca0061a6dd7d7ec1890e6da95d5d0839
Saurabh Kumar <saurabh9.in@gmail.com>
2026-08-25 23:46:28 +0530

    adding the search bar in frontend and with add project alignment

--- GitHubRepositoryExplorer/src/App.jsx
+++ GitHubRepositoryExplorer/src/App.jsx
@@ -1,15 +1,14 @@
-import SearchIcon from '@mui/icons-material/Search';
-import { Box, TextField } from '@mui/material'
+import { Route, Routes } from "react-router"
+import Home from "./pages/Home"
+
 
 const App = () => {
   return (
-    <div >
-      <Box sx={{ display: 'flex', alignItems: 'flex-end', cursor:"pointer" }}>
-      <TextField id="standard-basic" label="search" variant="standard" />
-      <SearchIcon />
-      </Box>
-      <div className='text-red-500 bg-gray-500'>dis</div>
-    </div>
+    <>
+      <Routes>
+        <Route index element={<Home />}/>
+      </Routes>
+    </>
   )
 }
 
---
+++ GitHubRepositoryExplorer/src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import SearchIcon from '@mui/icons-material/Search';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+const Hero = () => {
+    return (
+        <div>
+            <Box
+                component="form"
+                sx={{ '& > :not(style)': { m:1, width: '25ch' } }}
+                noValidate
+                autoComplete="off"
+                className='flex justify-center'
+            >
+
+                <TextField id="outlined-basic" label="Repository-Search" variant="outlined" />
+                <Button variant="contained">Contained</Button>
+            </Box>
+        </div>
+    )
+}
+
+export default Hero
